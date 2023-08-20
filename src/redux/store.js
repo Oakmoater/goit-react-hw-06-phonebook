a961dc1579d3e09e8057b7b616a8fbc4218dfc7d
Oakmoater <dubrovecva@gmail.com>
2023-08-20 15:35:31 +0200

    the end

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { contactsReducer } from "./contactsSlice";
 import { filterReducer } from "./filterSlice";
 import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import storage from 'redux-persist/lib/storage'
 
 const persistConfig = {
     key: 'root',
