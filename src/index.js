ded136e18c0a5a60cd07858ee1b9bf966f89c467
Oakmoater <dubrovecva@gmail.com>
2023-08-20 14:18:31 +0200

    persistor included

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { App } from "components/App";
-import { store } from "./redux/store";
+import { persistor, store } from "./redux/store";
 import "./index.css";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,35 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { contactsReducer } from "./contactsSlice";
 import { filterReducer } from "./filterSlice";
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 
-export const store = configureStore({
-    reducer: {
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const persistedReducer = persistReducer(
+    persistConfig,
+    {
         contacts: contactsReducer,
         filter: filterReducer,
-    },
+    });
+
+export const store = configureStore({
+    reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
+
+
+
+
+
+
+
+
+
+ 
+
+ 
